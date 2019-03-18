3b294d2b717189700469168fbddbb3f80870c4de
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-18 15:07:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -478,6 +478,8 @@ client.on('message', message => {
 %inv  //لاضافة البوت
 %shop //يجب تكون رتبة Seller وروم shop
 %myinvites  //لمعرفة كم عضو جبت
+%gmail //لارسال جيميل وهمي
+%giveaway //لبدء قيف اواي
 **
 `);
 
@@ -578,6 +580,124 @@ message.channel.send(`${user} has **${inviteCount}** invites.`);
 });
   }
 });
+
+
+client.on('message', dark => {
+    
+    if (dark.content === "Narox") {
+        setInterval(function(){
+        dark.edit('**N**')    
+        dark.edit('**Na**')    
+        dark.edit('**Nar**')
+        dark.edit('**Naro**')
+        dark.edit('**Narox**')
+        dark.edit('**Narox D**')
+        dark.edit('**Narox De*')
+        dark.edit('**Narox Dev**')
+        dark.edit('**Narox Devs**')
+        }, 900)
+    }
+    
+})
+
+
+
+client.on('message',async message => {//Narox
+  var time = moment().format('Do MMMM YYYY , hh:mm');
+  var room;
+  var title;
+  var duration;
+  var gMembers;
+  var currentTime = new Date(),//Narox
+hours = currentTime.getHours() + 3 ,
+minutes = currentTime.getMinutes(),
+done = currentTime.getMinutes() + duration / 60000 ,
+seconds = currentTime.getSeconds();
+if (minutes < 10) {//Narox
+minutes = "0" + minutes;
+}
+var suffix = "AM";//Narox
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;//Narox
+}
+ 
+  var filter = m => m.author.id === message.author.id;//Narox
+  if(message.content.startsWith(prefix + "giveaway")) {
+ 
+    if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
+    message.channel.send(`:eight_pointed_black_star:| **Send Name channel For the Giveaway**`).then(msg => {
+      message.channel.awaitMessages(filter, {//Narox
+        max: 1,//Narox
+        time: 20000,
+        errors: ['time']
+      }).then(collected => {//Narox
+        let room = message.guild.channels.find('name' , collected.first().content);//Narox
+        if(!room) return message.channel.send(':heavy_multiplication_x:| **i Found It :(**');//Narox
+        room = collected.first().content;
+        collected.first().delete();//Narox
+        msg.edit(':eight_pointed_black_star:| **Time For The Giveaway**').then(msg => {
+          message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 20000,//Narox
+            errors: ['time']
+          }).then(collected => {//Narox
+            if(isNaN(collected.first().content)) return message.channel.send(':heavy_multiplication_x:| **The Time Be Nambers `` Do the Commend Agin``**');
+            duration = collected.first().content * 60000;
+            collected.first().delete();//Narox
+            msg.edit(':eight_pointed_black_star:| **Now send The Present **').then(msg => {
+              message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 20000,//Narox
+                errors: ['time']
+              }).then(collected => {//Narox
+                title = collected.first().content;
+                collected.first().delete();
+                msg.delete();
+                message.delete();
+                try {
+                  let giveEmbed = new Discord.RichEmbed()
+                  .setDescription(`**${title}** \nReact With 🎉 To Enter! \nTime remaining : ${duration / 60000} **Minutes**\n **Created at :** ${hours}:${minutes}:${seconds} ${suffix}`)
+                  .setFooter(message.author.username, message.author.avatarURL);
+                  message.guild.channels.find("name" , room).send(' :heavy_check_mark: **Giveaway Created** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
+                     let re = m.react('🎉');//Narox
+                     setTimeout(() => {
+                       let users = m.reactions.get("🎉").users;//Narox
+                       let list = users.array().filter(u => u.id !== m.author.id !== client.user.id);//Narox
+                       let gFilter = list[Math.floor(Math.random() * list.length) + 0]
+                       let endEmbed = new Discord.RichEmbed()//Narox
+                       .setAuthor(message.author.username, message.author.avatarURL)
+                       .setTitle(title)//Narox
+                       .addField('Giveaway Ended !🎉',`Winners : ${gFilter} \nEnded at :`)
+                       .setTimestamp()
+                     m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
+                    message.guild.channels.find("name" , room).send(`**Congratulations ${gFilter}! You won The \`${title}\`**` , {embed: {}})
+                     },duration);//Narox
+                   });
+                } catch(e) {//Narox
+                message.channel.send(`:heavy_multiplication_x:| **i Don't Have Prem**`);
+                 console.log(e);//Narox
+               }
+             });
+           });
+         });
+       });
+     });
+   });
+ }
+});
+
+client.on("message", (message) => {
+    if(message.content.startsWith(prefix+"gmail")) {
+        message.channel.send(JSON.stringify({
+            email: Math.random().toString(36).slice(4).trim()+"@gmail.com",
+            password: Math.random().toString(36).slice(4).trim()
+        }))
+    }
+})
 
 
  
