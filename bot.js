2d21dbb069eb5a2a04e87e83bd9997dafe47ab93
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-18 13:07:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -385,6 +385,16 @@ m.sendMessage(args)
 }
 });
 
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+ 
+  if(date < 6) {
+    member.ban("Member account age is lower than 6 days.")
+  }
+});
+
  
  
  
