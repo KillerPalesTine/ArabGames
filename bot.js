2aeca58be76f38e22d542c2b507033a72834adb2
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-19 13:15:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -703,19 +703,6 @@ client.on("message", (message) => {
 })
 
 
-client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var Dark = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setTimestamp()
-        .setTitle('``! لقد تلقيت رساله جديدة في الخاص !``')
-        .setThumbnail(`${message.author.avatarURL}`)
-        .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-        .setFooter(`From ${message.author.tag} (${message.author.presence.status.toUpperCase()})`)
-    client.channels.get("557518887021969439").send({embed:Dark});
-    }
-});
 
 
 const devs = ["518816831734022154"]// ايدي الخاص بحسابك
