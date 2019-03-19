4771f46e6eb1fdcb32a2338332a3c42d0cd58b88
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-19 13:40:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,6 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`%help | ArabGames`,"http://twitch.tv/Death Shop")
-client.user.setGame(`%inv to invite the bot | ArabGames`,"http://twitch.tv/Death Shop")
 client.user.setStatus("online")
 });
  
@@ -246,60 +245,6 @@ client.on('message', message => {
     });
  
  
-client.on("message", (message) => {
- 
-   if (message.content.startsWith("%new")) {  
-        const reason = message.content.split(" ").slice(1).join(" ");  
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });  
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
-            const embed = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, #ticket`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-        }).catch(console.error);
-    } // كود التيكت كامل
- 
-  if (message.content.startsWith("%close")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
- 
-      message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتب.confirm`)
-          .then((m) => {
-              message.channel.awaitMessages(response => response.content === '$close', {
-                      max: 1,
-                      time: 10000,
-                      errors: ['time'],
-                  })  
-                  .then((collected) => {
-                      message.channel.delete();
-                  })  
-                  .catch(() => {
-                      m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                          m2.delete();
-                      }, 3000);
-                  });
-          });
-  }
- 
-});
 
 client.on('message', message => { //RayGamerMC MuteChannel Code
     var prefix = "%";
@@ -757,6 +702,33 @@ client.on('message', message => {
  
   });
 
+
+client.on('message', function(message) {//Narox
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;//Narox
+        var Narox = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTimestamp()//Narox
+        .setTitle('``I have received a new DM !``')
+        .setThumbnail(`${message.author.avatarURL}`)//Narox
+        .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+        .setFooter(`From **${message.author.tag} (${message.author.id})**`)
+    client.channels.get("557518887021969439").send({embed:Narox});//Narox
+
+     
+     client.on('guildMemberAdd', member => {
+    const botCount = member.guild.members.filter(m=>m.user.bot).size
+    const memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('557525815986880512').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
+    client.channels.get('557525835037409290').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+});
+
+client.on('guildMemberRemove', member => {
+    const botCount = member.guild.members.filter(m=>m.user.bot).size
+    const memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('557525815986880512').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
+    client.channels.get('557525835037409290').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+});
  
  
  
