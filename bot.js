0625ec1765b86761b06cd46017d2f4015280d35b
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-31 13:15:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : ArabGames ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`%help || ArabGames`,"http://twitch.tv/Death Shop")
+client.user.setGame(`%help | ArabGames`,"http://twitch.tv/Death Shop")
 client.user.setStatus("online")
 });
  
