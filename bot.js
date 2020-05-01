da464f089a93f91b051cb2944955217cf0f691a6
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2020-05-01 18:56:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-const fs = require("fs")
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const prefix = "%";
+.const fs = require("fs")
+.const Discord = require("discord.js");
+.const client = new Discord.Client();
+.const prefix = "%";
  
 
 
